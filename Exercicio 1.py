bf358268b380b5cc8b1675adff1c70a061bbea20
Bekolyof <kofsanha@gmail.com>
2026-05-04 21:27:23 -0300

    envio ativiade

--- Exercicio 1.py
+++ Exercicio 1.py
@@ -1,15 +1,13 @@
-nota1 = float(input("Digite a nota do 1 primeiro semestre: "))
-nota2 = float(input("Digite a nota do 2 segundo semestr: "))
-nota3= float(input("Digite a nota do 3 triceiro semestre: "))
+nota1 = float(input("Digite a nota do 1º semestre: "))
+nota2 = float(input("Digite a nota do 2º semestre: "))
+nota3 = float(input("Digite a nota do 3º semestre: "))
 
-media = (nota1 + nota2 + nota3/3)
-print(f"Media final:{media :.2f}")
+media = (nota1 + nota2 + nota3) / 3
+print(f"Média final: {media:.2f}")
 
-
-if media>= 7:
-    print("situação: Aprovado")
-if media>= 6:
-    print("situação: recuperação")
-if media>= 5: 
-    print("setução: reprovado")
-
+if media >= 7:
+    print("Situação: Aprovado")
+elif media >= 6:
+    print("Situação: Recuperação")
+else:
+    print("Situação: Reprovado")
